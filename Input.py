994fc9ca6c9fec66b7449e28ee4737b8d633a58a
stanavite <105546580+stanavite@users.noreply.github.com>
2023-10-29 17:37:49 -0400

    Add files via upload

---
+++ Input.py
@@ -0,0 +1,3 @@
+name = input("What is ur name?")
+age = input("How old are you?")
+print(" Hi " + name + " you are old: ")
---
+++ number.py
@@ -0,0 +1,9 @@
+#from math import *
+
+#num = 3.7
+#print(min(5,6))
+
+from math import *
+
+my_num = 7
+print(6 % 14)
