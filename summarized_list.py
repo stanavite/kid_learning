c6221ca3bc3aa78792a0f442ebdccd2e81e99048
stanavite <105546580+stanavite@users.noreply.github.com>
2023-12-03 17:09:32 -0500

    Add files via upload

---
+++ summarized_list.py
@@ -0,0 +1,18 @@
+friends = ["Bob," "Emo," "Alex,"]
+friends.append("Stanley")
+print(friends)
+friend2 = friends.copy()
+print(friend2)
+
+
+# append()
+# copy()
+# count()
+# insert()
+# reverse()
+# remove()
+# sort()
+# pop()
+# extend()
+# index()
+# clear()
